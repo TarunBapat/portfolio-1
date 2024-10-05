e6bf81153dd33602c4d882c8d0f9d0aba6d47255
tarunbapat <tarun.bapat710@gmail.com>
2024-10-05 17:34:01 +0530

    updates

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,34 +1,35 @@
-const lists=document.querySelectorAll('.alternate-style');
-const totalLinks=lists.length;
+const lists = document.querySelectorAll(".alternate-style");
+const totalLinks = lists.length;
 
-function setActiveStyle(color){
-	console.log(color);
-	for(let i=0;i<totalLinks;i++){
-		console.log(lists[i]);
-		if(color === lists[i].getAttribute('title')){
-			lists[i].removeAttribute('disabled');
-		}
-		else {
-			lists[i].setAttribute('disabled','true');
-		}
-	}
+function setActiveStyle(color) {
+  console.log(color);
+  for (let i = 0; i < totalLinks; i++) {
+    console.log(lists[i]);
+    if (color === lists[i].getAttribute("title")) {
+      lists[i].removeAttribute("disabled");
+    } else {
+      lists[i].setAttribute("disabled", "true");
+    }
+  }
 }
 
 /* Body Skin*/
 
-const bodySkin=document.querySelectorAll('.body-skin');
-const totalBodySkins=bodySkin.length;
-for(i=0;i<totalBodySkins;i++){
-	bodySkin[i].addEventListener('change',function(){
-		if(this.value==='dark'){
-			document.body.className='dark';
-		}
-		else {
-			document.body.className='';
-		}
-	})
+const bodySkin = document.querySelectorAll(".body-skin");
+console.log("bodySkin", bodySkin);
+const totalBodySkins = bodySkin.length;
+for (i = 0; i < totalBodySkins; i++) {
+  bodySkin[i].addEventListener("change", function () {
+    if (this.value === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  });
 }
 
-document.querySelector('.toggle-style-switcher').addEventListener('click', ()=>{
-	document.querySelector('.style-switcher').classList.toggle('open');
-})
+document
+  .querySelector(".toggle-style-switcher")
+  .addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+  });
